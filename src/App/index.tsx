d9fe0044a289e9609ecdfe551bda10f925de6b38
Himanshu Chauhan <himanshu.chauhan@sprinklr.com>
2021-08-07 18:22:54 +0530

    first commit

---
+++ src/App/index.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="cont-1">
+        <div className="text-1">
+          <p>Scroll Down</p>
+        </div>
+      </div>
+      <div className="no-image">
+        <div>
+          <h3>Parallax Demo</h3>
+          <p>
+            Parallax scrolling is a web site trend where the background content
+            is moved at a different speed than the foreground content while
+            scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis
+            nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit
+            pellentesque a, magna turpis est sapien duis blandit dignissim.
+            Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum
+            consequat morbi, curabitur aliquam pede, nullam vitae eu placerat
+            eget et vehicula. Varius quisque non molestie dolor, nunc nisl
+            dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus
+            pulvinar, in in neque risus odio. Accumsan fringilla vulputate at
+            quibusdam sociis eleifend, aenean maecenas vulputate, non id
+            vehicula lorem mattis, ratione interdum sociis ornare. Suscipit
+            proin magna cras vel, non sit platea sit, maecenas ante augue etiam
+            maecenas, porta porttitor placerat leo.
+          </p>
+        </div>
+      </div>
+      <div className="cont-2">
+        <div className="text-1">
+          <p>Scroll Down</p>
+        </div>
+      </div>
+      <div className="no-image">
+        <div>
+          <h2>Parallax Demo</h2>
+          <p>
+            Parallax scrolling is a web site trend where the background content
+            is moved at a different speed than the foreground content while
+            scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis
+            nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit
+            pellentesque a, magna turpis est sapien duis blandit dignissim.
+            Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum
+            consequat morbi, curabitur aliquam pede, nullam vitae eu placerat
+            eget et vehicula. Varius quisque non molestie dolor, nunc nisl
+            dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus
+            pulvinar, in in neque risus odio. Accumsan fringilla vulputate at
+            quibusdam sociis eleifend, aenean maecenas vulputate, non id
+            vehicula lorem mattis, ratione interdum sociis ornare. Suscipit
+            proin magna cras vel, non sit platea sit, maecenas ante augue etiam
+            maecenas, porta porttitor placerat leo.
+          </p>
+        </div>
+      </div>
+      <div className="cont-3">
+        <div className="text-1">
+          <p>Scroll Down</p>
+        </div>
+      </div>
+      <div className="no-image">
+        <div>
+          <h3>Parallax Demo</h3>
+          <p>
+            Parallax scrolling is a web site trend where the background content
+            is moved at a different speed than the foreground content while
+            scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis
+            nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit
+            pellentesque a, magna turpis est sapien duis blandit dignissim.
+            Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum
+            consequat morbi, curabitur aliquam pede, nullam vitae eu placerat
+            eget et vehicula. Varius quisque non molestie dolor, nunc nisl
+            dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus
+            pulvinar, in in neque risus odio. Accumsan fringilla vulputate at
+            quibusdam sociis eleifend, aenean maecenas vulputate, non id
+            vehicula lorem mattis, ratione interdum sociis ornare. Suscipit
+            proin magna cras vel, non sit platea sit, maecenas ante augue etiam
+            maecenas, porta porttitor placerat leo.
+          </p>
+        </div>
+      </div>
+      <div className="cont-4">
+        <div className="text-1">
+          <p>Scroll Down</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
